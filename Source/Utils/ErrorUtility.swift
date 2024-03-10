1d9b7eae41c57ea74177385d178fa76b0ac2361b
Ajaya Mati <ajaymati1000@gmail.com>
2024-03-10 13:03:07 +0530

    Error Utility file & line number

--- Source/Utils/ErrorUtility.swift
+++ Source/Utils/ErrorUtility.swift
@@ -12,7 +12,19 @@ class ErrorUtility {
     
     private init() {}
     
-    func fatalError(msg: String) -> Never {
-        Swift.fatalError("XConstraintKit: \(#file) \(#line) \(msg)")
+    func fatalError(msg: String, file: String = #file, line: Int = #line) -> Never {
+        Swift.fatalError(getErrorString(msg, file: file, line: line))
+    }
+}
+
+
+extension ErrorUtility {
+    private func getErrorString(_ msg: String, file: String, line: Int) -> String {
+        """
+        \(msg)
+        in
+        \(file)
+        on Line: \(line)
+        """
     }
 }
